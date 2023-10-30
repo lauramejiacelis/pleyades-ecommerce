eeb45c3b689b625e477347f5d41ac2a4f4aeb779
lauramejiacelis <lauram2celis@gmail.com>
2023-10-29 21:18:40 -0500

    Backend Authentication

--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 const port = process.env.PORT || 5000;
 
 connectDB();  // Connect to MongoDB
 
 const app = express()
+
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+//Cookie parser middleware
+app.use(cookieParser())
 
 //Routes
 app.get('/',(req,res)=>{
@@ -16,6 +25,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //Error middlewares
 app.use(notFound)
