bdec205da0f93717c7b84ae2c0f7346b9e5d6531
lauramejiacelis <lauram2celis@gmail.com>
2023-11-12 16:44:41 -0500

    prepare for production

--- api/server.js
+++ api/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 
 //Routes
-app.get('/',(req,res)=>{
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -37,6 +33,21 @@ app.get('/api/config/paypal', (req,res)=> res.send({ clientId: process.env.PAYPA
 //Make uploads folder static
 const __dirname= path.resolve(); // set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+//Validation for Production
+if (process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  //any route that is not api will be redirected to index.html
+  app.get('*', (req, res)=>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/',(req,res)=>{
+    res.send('API is running...')
+  })
+}
 
 //Error middlewares
 app.use(notFound)
